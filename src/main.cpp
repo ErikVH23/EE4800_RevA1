84cbab0e7b4854631e4d419ea05e39c0feb9d834
ErikVH23 <eavanor@mtu.edu>
2023-04-27 01:22:07 -0400

    initial commit of cli script

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,19 @@
 #define PUL 10
 #define DIR 11
 #define ENA 12
+#define ORIGIN_LIMIT_SWITCH 5
+#define ENDPOINT_LIMIT 1792000
+#define SPEED_LIMIT 16000
 
-#define FORWARD_SPEED 800
-#define REVERSE_SPEED -800
+int ENDPOINT = ENDPOINT_LIMIT;
+int ROTATION_SPEED = 15000;
+int FORWARD_SPEED = 800;
+int REVERSE_SPEED = -800;
+int POSITION = 0;
+int stepCount = 0;
+bool PULSE = false;
 
-#define ENDPOINT 1792000
 Stepper myStepper(400,PUL, DIR);
-
-  int POSITION = 0;
-  int stepCount = 0;
-  bool PULSE = false;
-
 
 void setup() {
 
@@ -28,6 +30,64 @@ void setup() {
 
   // put your setup code here, to run once:
 }
+
+
+//--------------------------------------------------------------
+
+bool setEndPoint(int endpointStepCount){
+  if((endpointStepCount > ENDPOINT_LIMIT) || (endpointStepCount <= 0))
+  {
+    return false;
+  }
+  else
+  {
+    ENDPOINT = endpointStepCount;
+    return true;
+  }
+
+  return true;
+}
+
+//--------------------------------------------------------------
+
+bool setSpeed(int speed){
+  if((speed > SPEED_LIMIT) || (speed <= 0))
+  {
+    return false;
+  }
+  else
+  {
+    ROTATION_SPEED = speed;
+    return true;
+  }
+}
+
+//--------------------------------------------------------------
+
+bool setReverseSpeed(int speed){
+
+  return true;
+}
+
+//--------------------------------------------------------------
+
+int returnToOrigin(){
+
+  while(true)
+  {
+    myStepper.setSpeed(REVERSE_SPEED);
+
+    if(digitalRead(ORIGIN_LIMIT_SWITCH) == LOW)
+    {
+      POSITION = 0;
+      return 1;
+    }
+  }
+}
+
+//--------------------------------------------------------------
+
+
 
 void loop() {
 
